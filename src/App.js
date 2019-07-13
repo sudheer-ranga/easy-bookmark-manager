ad72ef9c7a0ba54462adee85b84254408feb42c6
Sudheer Ranga <sudheer.ranga1@gmail.com>
2019-07-14 00:37:11 +0530

    change things

--- src/App.js
+++ src/App.js
@@ -20,64 +20,12 @@ function App() {
     return dom;
   };
 
-  // const getFolders = bookmarks => {
-  //   let dom = Object.keys(bookmarks).map(key => {
-
-  //     if (!bookmarks[key].url) {
-  //       return (
-  //         <AccordionItem title={bookmarks[key].title}></AccordionItem>
-  //       );
-  //     }
-  //   });
-
-  //   dom = <Accordion atomic={false}>{dom}</Accordion>;
-  //   return dom;
-  // };
-
-  // if (bookmarks[key].children) {
-  //   let nestedDom = getFolders(bookmarks[key].children);
-
-  //   innerDom = <Accordion atomic={false}>{nestedDom}</Accordion>;
-  // }
-
-  // let sidebarDom = getFolders(bookmarks[0].children);
-
-  // const theBookmarks = bookmarks[0].children;
-
   return (
     <div class="bookmark-container">
       <div class="sidebar">
         <Accordion atomic={false}>
           {getFolders(bookmarks[0].children)}
         </Accordion>
-        {/* <div dangerouslySetInnerHTML={{ __html: sidebarDom }} /> */}
-
-        {/* <Accordion atomic={false}>
-          <AccordionItem title="Title 1">
-            <p>Hello 1</p>
-          </AccordionItem>
-
-          <AccordionItem title="Title 2">
-            <p>Hello 2</p>
-          </AccordionItem>
-
-          <AccordionItem title="Title 3">
-            <p>Hello 3</p>
-          </AccordionItem>
-        </Accordion> */}
-        {/* <Accordion atomic={false}>
-          {Object.keys(theBookmarks).map(key => {
-            if (!theBookmarks[key].url) {
-              return (
-                <AccordionItem title={theBookmarks[key].title}>
-                  {theBookmarks[key].children
-                    ? getFolders(theBookmarks[key].children)
-                    : ''}
-                </AccordionItem>
-              );
-            }
-          })}
-        </Accordion> */}
       </div>
       <div class="content">
         <p>
