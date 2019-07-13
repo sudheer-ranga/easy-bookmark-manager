da1325a7506c966df746c9b7e2d3784242895519
Sudheer Ranga <sudheer.ranga1@gmail.com>
2019-07-14 00:21:49 +0530

    accordion

--- src/App.js
+++ src/App.js
@@ -1,33 +1,83 @@
 import React from 'react';
 import './App.css';
+import { Accordion, AccordionItem } from 'react-light-accordion';
+import 'react-light-accordion/demo/css/index.css';
 
 function App() {
   const bookmarks = window.bookmarks;
-  let dom = '';
 
   const getFolders = bookmarks => {
-    Object.keys(bookmarks).forEach(key => {
+    let dom = Object.keys(bookmarks).map(key => {
       if (!bookmarks[key].url) {
-        dom += `<li>${bookmarks[key].title}</li>`;
-        console.log(bookmarks[key].title);
-      }
-
-      if (bookmarks[key].children) {
-        dom += '<ul>';
-        getFolders(bookmarks[key].children);
-        dom += '</ul>';
+        return (
+          <AccordionItem title={bookmarks[key].title}>
+            {bookmarks[key].children ? getFolders(bookmarks[key].children) : ''}
+          </AccordionItem>
+        );
       }
     });
+
+    return dom;
   };
 
-  getFolders(bookmarks[0].children);
+  // const getFolders = bookmarks => {
+  //   let dom = Object.keys(bookmarks).map(key => {
 
-  dom = `<ul>${dom}</ul>`;
+  //     if (!bookmarks[key].url) {
+  //       return (
+  //         <AccordionItem title={bookmarks[key].title}></AccordionItem>
+  //       );
+  //     }
+  //   });
+
+  //   dom = <Accordion atomic={false}>{dom}</Accordion>;
+  //   return dom;
+  // };
+
+  // if (bookmarks[key].children) {
+  //   let nestedDom = getFolders(bookmarks[key].children);
+
+  //   innerDom = <Accordion atomic={false}>{nestedDom}</Accordion>;
+  // }
+
+  // let sidebarDom = getFolders(bookmarks[0].children);
+
+  // const theBookmarks = bookmarks[0].children;
 
   return (
     <div class="bookmark-container">
       <div class="sidebar">
-        <div dangerouslySetInnerHTML={{ __html: dom }} />
+        <Accordion atomic={false}>
+          {getFolders(bookmarks[0].children)}
+        </Accordion>
+        {/* <div dangerouslySetInnerHTML={{ __html: sidebarDom }} /> */}
+
+        {/* <Accordion atomic={false}>
+          <AccordionItem title="Title 1">
+            <p>Hello 1</p>
+          </AccordionItem>
+
+          <AccordionItem title="Title 2">
+            <p>Hello 2</p>
+          </AccordionItem>
+
+          <AccordionItem title="Title 3">
+            <p>Hello 3</p>
+          </AccordionItem>
+        </Accordion> */}
+        {/* <Accordion atomic={false}>
+          {Object.keys(theBookmarks).map(key => {
+            if (!theBookmarks[key].url) {
+              return (
+                <AccordionItem title={theBookmarks[key].title}>
+                  {theBookmarks[key].children
+                    ? getFolders(theBookmarks[key].children)
+                    : ''}
+                </AccordionItem>
+              );
+            }
+          })}
+        </Accordion> */}
       </div>
       <div class="content">
         <p>
