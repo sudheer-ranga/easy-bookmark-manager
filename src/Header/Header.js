bb8817b771bd1c62ea13bd361b918001fae387e8
Sudheer Ranga <sudheer.ranga1@gmail.com>
2019-07-16 03:53:53 +0530

    Modal with search working

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -6,8 +6,16 @@ import { debounce, searchJson } from '../util';
 function Header() {
   const [appState] = useContext(BookmarkContext);
   const [searchResults, setSearchResults] = useState([]);
+  const [searchModalOpened, setSearchModalOpened] = useState(false);
 
-  const openSearchModal = () => {};
+  const openSearchModal = () => {
+    setSearchModalOpened(true);
+  };
+
+  const closeSearchModal = () => {
+    setSearchModalOpened(false);
+    setSearchResults([]);
+  };
 
   const searchBookmarks = e => {
     // debugger;
@@ -36,28 +44,30 @@ function Header() {
         </p>
       </div>
 
-      <div className="search-modal">
-        <div className="close-modal">
-          <span>x</span>
+      {searchModalOpened && (
+        <div className="search-modal">
+          <div className="close-modal">
+            <span onClick={closeSearchModal}>x</span>
+          </div>
+          <div className="search-block">
+            <input
+              type="search"
+              className="modal-search-input"
+              placeholder="Search Bookmarks"
+              onChange={searchBookmarks}
+            />
+          </div>
+          <ul className="search-result-block">
+            {searchResults.map((result, index) => (
+              <li className="search-result-item" key={index}>
+                <a href={result.url} target="_blank" rel="noopener noreferrer">
+                  {result.title}
+                </a>
+              </li>
+            ))}
+          </ul>
         </div>
-        <div className="search-block">
-          <input
-            type="search"
-            className="modal-search-input"
-            placeholder="Search Bookmarks"
-            onChange={searchBookmarks}
-          />
-        </div>
-        <ul className="search-result-block">
-          {searchResults.map((result, index) => (
-            <li className="search-result-item" key={index}>
-              <a href={result.url} target="_blank" rel="noopener noreferrer">
-                {result.title}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </div>
+      )}
     </div>
   );
 }
